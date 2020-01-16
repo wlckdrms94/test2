1d2a32cf8d1662d241fde731a2c1a70b9449375d
wlckdrms94 <59040486+wlckdrms94@users.noreply.github.com>
2020-01-16 19:43:56 +0900

    수정
    3

---
+++ project/src/container/WebtoonHome.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+import Header from "../component/Header";
+import Gnb from "../component/Gnb";
+import Footer from "../component/Footer";
+
+class WebtoonHome extends Component{
+    render(){
+        return(
+            <div>
+                <Header/>
+                <Gnb/>
+                WebtoonHome
+                <Footer/>
+            </div>
+        )
+    }
+}
+
+export default WebtoonHome;
